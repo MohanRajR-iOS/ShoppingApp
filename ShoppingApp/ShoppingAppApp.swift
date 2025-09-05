a4cd14e7405398d18729ebd7236f487898eb09d7
Mohan Raj R <mjmohan1995@gmail.com>
2025-09-05 12:47:49 +0530

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Mohan raj on 05/09/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ShoppingAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
